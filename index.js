4f52d2e7a1e6f46c567377ce61b5735a3977a3fd
苏梦莹 <353090398@qq.com>
2016-09-27 16:48:58 +0800

    post

--- index.js
+++ index.js
@@ -2,22 +2,18 @@ var express = require('express');
 var app = express();
 
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/', function(req, res) {
+app.get('/:name', function(req, res) {
+var userName = req.params.name
 var page = '<html>' +
               '<body>' +
-                '<h1>index.html</h1>' +
+                '<h1>' + userName + '的购物车' + '</h1>' +
               '</body>' +
            '</html>'
 res.send(page)
 });
 
-app.get('/about', function(req, res) {
-  var page = '<html>' +
-                '<body>' +
-                  '<h1>about.html</h1>' +
-                '</body>' +
-             '</html>'
-  res.send(page)
+app.post('/:name', function(req, res) {
+  res.send('哈哈哈！成功了' + req.params.name)
 });
 
 app.listen(3000,function(){
