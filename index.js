f88e44734c45a7655c659fa427d86ee3ae40155c
苏梦莹 <353090398@qq.com>
2016-09-27 15:45:47 +0800

    m

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ var express = require('express');
 var app = express();
 
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/dd', function(req, res) {
-  console.log('hello didi');
+app.get('/', function(req, res) {
+var page = '<html>' +
+              '<body>' +
+                '<h1>index.html</h1>' +
+              '</body>' +
+           '</html>'
+res.send(page)
 });
 
-app.get('/ss', function(req, res) {
-  console.log('hello su');
+app.get('/about', function(req, res) {
+  var page = '<html>' +
+                '<body>' +
+                  '<h1>about.html</h1>' +
+                '</body>' +
+             '</html>'
+  res.send(page)
 });
 
 app.listen(3000,function(){
