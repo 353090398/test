09d9cc16169aa741d48dab5fbd65ab103804009a
苏梦莹 <353090398@qq.com>
2016-09-30 11:18:06 +0800

    j

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express();
 
-// respond with "hello world" when a GET request is made to the homepage
 app.get('/:name', function(req, res) {
 var userName = req.params.name
 var page = '<html>' +
@@ -15,6 +14,26 @@ res.send(page)
 app.post('/:name', function(req, res) {
   res.send('哈哈哈！成功了' + req.params.name)
 });
+
+app.get('/posts', function(req, res) {
+  console.log("GET /posts");
+});
+
+// app.get('/posts/:posts_id', function(req, res) {
+//   console.log("GET/posts/:posts_id");
+// });
+//
+// app.post('/posts', function(req, res) {
+//   console.log("POST/posts");
+// });
+//
+// app.put('/posts/:posts_id', function(req, res) {
+//   console.log("PUT/posts/:posts_id");
+// });
+//
+// app.delete('/posts/:posts_id', function(req, res) {
+//   console.log("DELETE /posts/:posts_id");
+// });
 
 app.listen(3000,function(){
   console.log('running on prot 3000...');
