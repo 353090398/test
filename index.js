eabc7b25b1a2dc63bd359577440966c37f69fe7b
苏梦莹 <353090398@qq.com>
2016-10-03 10:24:55 +0800

    s

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 var app = express();
-
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 // app.get('/:name', function(req, res) {
 // var userName = req.params.name
 // var page = '<html>' +
@@ -14,22 +16,28 @@ var app = express();
 // app.post('/:name', function(req, res) {
 //   res.send('哈哈哈！成功了' + req.params.name)
 // });
-
 // app.get('/posts', function(req, res) {
-//   res.send("GET /posts")
-//   console.log("GET /posts");
+//   res.send("GET /posts"))
+  // console.log("GET /posts");
 // });
-//
+
 // app.get('/posts/:post_id', function(req, res) {
 //   res.send("GET/post/:posts_id")
 //   console.log("GET/post/:posts_id");
 // });
-//
-// app.post('/posts', function(req, res) {
-//   res.send("POST/posts")
-//   console.log("POST/posts");
-// });
-//
+
+app.get('/write',function(req,res){
+  var page = "<form method='post' action='/posts'>" +
+  "<input type='text' name='title'/>" +
+  "<input type='submit'/>" +
+  "</from>"
+  res.send(page)
+})
+app.post('/posts', function(req, res) {
+  res.send("the post title is: " + req.body.title)
+  // console.log("POST /posts");
+});
+
 // app.put('/posts/:post_id', function(req, res) {
 //   res.send("PUT/posts/:post_id")
 //   console.log("PUT/posts/:post_id");
